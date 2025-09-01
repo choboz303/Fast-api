6523daf107c9ee4312aed03239c707ff8b3e9cf5
choboz <choboz.303@gmail.com>
2025-09-01 10:44:49 +0900

    cors origin for vite

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from schemas import CsrfSettings, SuccessMsg
 app = FastAPI()
 app.include_router(route_todo.router)
 app.include_router(route_auth.router)
-origins = ["http://localhost:3000"]
+origins = ["http://localhost:5173"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
